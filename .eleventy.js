ad14d124736f1b1af2bd969b9ace32cb4acf1f70
bayfujimoto <bayfujimoto@gmail.com>
2026-01-09 13:26:31 -0600

    AddPassthroughCopy for _includes/assets

--- .eleventy.js
+++ .eleventy.js
@@ -238,6 +238,9 @@ eleventyConfig.addShortcode("generateImage", async function(params) {
   eleventyConfig.addPassthroughCopy({"assets/icons/favicon.svg" : "/favicon.svg"});
   eleventyConfig.addPassthroughCopy("projects/**/*.{jpg,jpeg,png,gif,webp,svg,avif}");
 
+  // Copy assets folder to output
+  eleventyConfig.addPassthroughCopy("_includes/assets");
+
   // Disable 11ty dev server live reload when using CMS locally
   eleventyConfig.setServerOptions({
     liveReload: false
